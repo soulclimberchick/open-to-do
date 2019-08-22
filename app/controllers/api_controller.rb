4df70ebc865c846ef27e7930e749fbfafa88ed15
Kira Helm <soulclimberchick@gmail.com>
2019-08-22 11:27:53 -0700

    User story - authenticating via command line completed

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,13 @@
+class ApiController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
+  private
+
+  def authenticated?
+    authenticate_or_request_with_http_basic do |name, password|
+      user = User.find_by_name(name)
+      user && user.authenticate(password)
+    end
+  end
+end
