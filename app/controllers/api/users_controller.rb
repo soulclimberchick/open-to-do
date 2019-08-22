d50c3932e5cabf31d8c8b123a0a6af753f859a83
Kira Helm <soulclimberchick@gmail.com>
2019-08-22 16:04:57 -0700

    partial commit getting stuck on adding users via curl

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,4 @@
 class Api::UsersController < ApiController
-
   before_action :authenticated?
 
   def index
@@ -7,4 +6,18 @@ class Api::UsersController < ApiController
     render json: @users, each_serializer: UserSerializer
   end
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,17 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'welcome/about'
 
-  namespace :api, defaults: { format: :json } do
-    resources :users
+namespace :api, defaults: { format: :json } do
+  resources :users do
+    resources :lists
   end
+
+  resources :lists, only: [] do
+    resources :items, only: [:create]
+  end
+
+  resources :items, only: [:destroy]
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index'
 end
