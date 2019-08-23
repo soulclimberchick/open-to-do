1dae448c338252a80357001ec9b5a40ef54552a0
Kira Helm <soulclimberchick@gmail.com>
2019-08-23 13:41:15 -0700

    Added ability to delete both users and lists

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -12,7 +12,17 @@ class Api::ListsController < ApiController
     if list.save
       render json: list
     else
-      render json: { errors: list.erros.full_messages }, status: :unprocessable_entity
+      render json: { errors: list.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    begin
+      list = List.find(params[:id])
+      list.destroy
+      render json: {}, status: :no_content
+    rescue ActiveRecord::RecordNotFound
+      render :json => {}, :status => :not_found
     end
   end
 
